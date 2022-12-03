a1eeec446765df61a16a1a50a852607843f024b5
“akashmane3938” <maneakash3938@gmail.com>
2022-12-03 02:52:19 -0800

    Line Comparision

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lines_Pratice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            L1_Calculate_Length.Length();
+            Console.ReadLine();
+
+
+          //  L2_Equal_Lines.Equal();
+          //  Console.ReadLine();
+
+          //  L3_Greater_Less.Greater();
+          //  Console.ReadLine();
+        }
+    }
+}
